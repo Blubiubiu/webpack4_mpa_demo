4e932b1dbd755665d884ce3eeea4cb850cb7dbf5
taoxm310 <taoxiumin2016@gmail.com>
2019-07-26 17:32:17 +0800

    close #9

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -10,16 +10,15 @@ const webpackConfigBase = require('./webpack.base.conf');
 
 const webpackConfigProd = {
     mode: 'production', // 通过 mode 声明生产环境
-    
+
 	output: {
 		path: path.resolve(__dirname, '../dist'),
 		// 打包多出口文件
 		filename: './js/[name].[hash].js',
-		publicPath: './'
     },
-    
+
     devtool: 'cheap-module-eval-source-map',
-    
+
 	plugins: [
 		//删除dist目录
 		new cleanWebpackPlugin(['dist'], {
