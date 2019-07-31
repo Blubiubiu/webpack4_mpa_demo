88291ecd5f65db7fedd3b81d83693e83fa61b13f
Bali <lisiyuan@tengyue360.com>
2019-07-31 11:09:55 +0800

    delete px2rem

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,8 @@
 module.exports = {
 	plugins: [
 		//自动添加css前缀
-        require('autoprefixer'),
-        require("postcss-plugin-px2rem")({'remUnit': 75,'baseDpr':2})
+        require('autoprefixer')
+        //转换rem  需 install postcss-plugin-px2rem
+        // require("postcss-plugin-px2rem")({'remUnit': 75,'baseDpr':2})
 	]
 };
